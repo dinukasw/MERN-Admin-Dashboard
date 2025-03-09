34dd9a16b30a36580b0c596e88a55142d62fa17a
dinukasw <dinukasanthush50@gmail.com>
2025-03-09 23:17:52 +0530

    complete sidebar with list items

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -2,13 +2,17 @@ import React,{ useState } from 'react';
 import  { Box, useMediaQuery } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import NavBar from '@/components/NavBar';
+import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
 
 const Layout = () => {
+  const isNonMobile = useMediaQuery('(min-width: 600px)');
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   return (
-    <Box width="100%" height="100%">
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar isNonMobile={isNonMobile} drawerWidth =" 240px" isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
       <Box>
-        <NavBar />
+        <Navbar isSidebarOpen={isSidebarOpen} setIsSidebarOpen= {setIsSidebarOpen} />
         <Outlet />
       </Box>
     </Box>
