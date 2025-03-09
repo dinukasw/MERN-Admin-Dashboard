6254e871ec1c9d19b41bbaa24fb167af03a8a2f3
dinukasw <dinukasanthush50@gmail.com>
2025-03-09 19:18:13 +0530

    create redux for theme settings and update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useSelector, useMemo } from 'react'
+import { CssBaseline, ThemeProvider } from '@mui/material/styles'
+import { createTheme } from '@mui/material'
+import { themeSettings } from 'theme'
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
-    <div>
-      
+    <div className='app'>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <h1>App</h1>
+      </ThemeProvider > 
     </div>
   )
 }
